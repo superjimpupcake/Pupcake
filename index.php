6b0b563d568c26896399b48bac1dcb618d282ce3
Zike(Jim) Huang <jimpupcakeframeworkgithub@gmail.com>
2012-05-31 17:47:47 -0700

    documentation updates

--- index.php
+++ index.php
@@ -3,46 +3,8 @@ require "pupcake.php";
 
 $app = new \Pupcake\Pupcake();
 
-$app->get("/", function(){
-    return "default response";
-});
-
-$app->get("/hello/:name", function($name){
-    return $name;
-});
-
-$app->post("/hello/:name", function($name){
-    return "posting $name to hello";
-});
-
-$app->get("test", function() use ($app){
-    return $app->redirect("test2");
-});
-
-$app->any("/:year/:month/:day", function($year, $month, $day){
-    return $year."-".$month."-".$day;
-});
-
-$app->get("/test2", function(){
-    return "testing 2";
-});
-
-$app->any("*", function(){
-    return "all routes finally go here";
-});
-
-$app->notFound(function(){
-    return "not found any routes!";
-});
-
-$app->get("test_internal", function() use ($app) {
-    $content = "";
-    $content .= $app->forward("POST", "hello/world")."<br/>";
-    $content .= $app->forward("GET", "hello/world2")."<br/>";
-    $content .= $app->forward("GET", "hello/world3")."<br/>";
-    $content .= $app->forward("GET", "test")."<br/>";
-    $content .= $app->forward("POST", "2012/05/30")."<br/>";
-    return $content;
-});
+/**
+ * for detail usage, please refer to readme.md
+ */
 
 $app->run();
