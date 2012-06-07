6734b55c37d9f9f4a4cef280f1c3929e2e398cf3
Zike(Jim) Huang <jimpupcakeframeworkgithub@gmail.com>
2012-06-07 00:34:22 -0700

    add storageSet and storageGet to base object

--- src/Pupcake/Object.php
+++ src/Pupcake/Object.php
@@ -3,8 +3,14 @@ namespace Pupcake;
 
 class Object
 {
+    private $methods; //store all undeclared object methods
+    private $storage; //built-in object storage
 
-    private $methods;
+    public function __construct()
+    {
+        $this->methods = array();
+        $this->storage= array();
+    }
 
     public static function instance()
     {
@@ -19,7 +25,7 @@ class Object
     {
         $class_name = get_class($this);
         if(isset($this->methods[$class_name])){
-           return call_user_func_array($this->methods[$class_name], $params); 
+            return call_user_func_array($this->methods[$class_name], $params); 
         }
     }
 
@@ -28,5 +34,18 @@ class Object
         $class_name = get_called_class(); 
         $this->methods[$class_name] = $callback;
     }
-}
 
+    final public function storageSet($key, $val)
+    {
+        $this->storage[$key] = $val;
+    }
+
+    final public function storageGet($key)
+    {
+        $result = null;
+        if(isset($this->storage[$key])){
+            $result = $this->storage[$key];
+        }
+        return $result;
+    }
+}
