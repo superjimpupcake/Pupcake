9a75df7b0a7ffabac60a9423da7b354bca33e7a7
Zike(Jim) Huang <jimpupcakeframeworkgithub@gmail.com>
2012-06-08 08:19:03 -0700

    refactor express service, use class structure instead of dynamic method creation

--- src/Pupcake/Service/Express.php
+++ src/Pupcake/Service/Express.php
@@ -6,36 +6,63 @@ namespace Pupcake\Service;
 
 use Pupcake;
 
+class ExpressRequest extends Pupcake\Object
+{
+    private $route;
+
+    public function __construct($route)
+    {
+        $this->route = $route;
+    }
+
+    public function params($param_name)
+    {
+        $params = $this->route->getParams();
+        $result = "";
+        if(isset($params[$param_name])){
+            $result = $params[$param_name];
+        }
+        return $result;
+
+    }
+}
+
+class ExpressResponse extends Pupcake\Object
+{
+    private $app;
+    private $route;
+
+    public function __construct($app, $route)
+    {
+        $this->app = $app;
+        $this->route = $route;
+    }
+
+    public function send($output)
+    {
+        $this->route->storageSet('output', $output); 
+    }
+
+    public function redirect($uri)
+    {
+        $this->app->redirect($uri);
+    }
+
+    public function forward($request_type, $uri)
+    {
+        $this->app->forward($request_type, $uri);
+        $route = $this->app->getRouter()->getMatchedRoute();
+        return $route->storageGet('output');
+    }
+}
+
 class Express extends Pupcake\Service
 {
     public function start($app)
     {
         $app->on("system.request.found", function($route) use ($app) {
-            $req = new Pupcake\Object();
-            $res = new Pupcake\Object();
-            $req->method('params', function($param_name) use ($route){
-                $params = $route->getParams();
-                $result = "";
-                if(isset($params[$param_name])){
-                    $result = $params[$param_name];
-                }
-                return $result;
-            });
-
-            $res->method('send', function($output) use ($route){
-               $route->storageSet('output', $output); 
-            });
-
-            $res->method('redirect', function($uri) use($app) {
-                $app->redirect($uri);
-            });
-
-            $res->method('forward', function($request_type, $uri) use($app, $req, $res) {
-                $app->forward($request_type, $uri);
-                $route = $app->getRouter()->getMatchedRoute();
-                return $route->storageGet('output');
-            });
-
+            $req = new ExpressRequest($route);
+            $res = new ExpressResponse($app, $route);
             call_user_func_array($route->getCallback(), array($req, $res));
             return $route->storageGet('output');
         });
