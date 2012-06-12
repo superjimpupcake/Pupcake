072285027108fe17be8caa2ac5d488b73dba44d6
Zike(Jim) Huang <jimpupcakeframeworkgithub@gmail.com>
2012-06-12 10:22:50 -0700

    fix params passed to express service

--- src/Pupcake/Service/Express.php
+++ src/Pupcake/Service/Express.php
@@ -10,7 +10,7 @@ class Express extends Pupcake\Service
 {
     public function start($app, $config = array())
     {
-        $this->on("system.request.found", function($event, $handler) use ($app) {
+        $this->on("system.request.found", function($event) use ($app) {
             $route = $event->props('route');
             $req = new Express\Request($route);
             $res = new Express\Response($app, $route);
