7596743fb176ba73883113e081ae27559232e7b0
Zike(Jim) Huang <jimpupcakeframeworkgithub@gmail.com>
2012-06-14 00:23:31 -0700

    add test case for building service event handlers on top of existing ones

--- tests/Pupcake/Tests/CustomEventRegisterTest.php
+++ tests/Pupcake/Tests/CustomEventRegisterTest.php
@@ -55,4 +55,55 @@ class CustomEventRegisterTest extends Pupcake\TestCase
         }
 
     }
+
+    public function testCustomServiceCallbackOnTopOfExistingOnes()
+    {
+        $expectations = array();
+        $expectations[] = array('input' => '192.168.2.1', 'output' => 'api#iphello world');
+        $expectations[] = array('input' => '10.0.0.1', 'output' => 'api#iphello world');
+        $expectations[] = array('input' => 'random', 'output' => 'Invalid Request');
+
+        foreach($expectations as $expectation){
+            $this->simulateRequest("get", "/api/ip/".$expectation['input']);
+
+            $app = new Pupcake\Pupcake();
+            
+            $services = array();
+            $services['express'] = $app->getService("Pupcake\Service\Express"); //load Express service
+            $services['constraint'] = $app->getService("Pupcake\Service\RouteConstraint"); //load RouteConstraint service
+            $services['action'] = $app->getService("Pupcake\Service\RouteAction"); //load RouteAction service
+
+            // custom event registration
+            $app->on("system.routing.route.create", function($event) use ($services) {
+                $service_callbacks = array(
+                    function($event){
+                        $event->props('route')->method('hello', function($word){
+                            return "hello $word";
+                        }); 
+                    }
+                );
+                $service_callbacks = array_merge($services, $service_callbacks);
+                return call_user_func_array( array($event, "register"), $service_callbacks)->start();
+            });
+
+            $app->get("api/ip/:ip", function($req, $res) use ($app) {
+                $route = $app->getRouter()->getMatchedRoute();
+                $action = $route->getAction();
+                $hello = $route->hello('world');
+                $res->send($action.$hello);
+            })
+            ->to("api#ip")
+            ->constraint(array(
+                'ip' =>  function($value){
+                    return \Respect\Validation\Validator::ip()->validate($value);
+                }
+            ));
+
+            $app->run();
+
+            $this->assertEquals($this->getRequestOutput(), $expectation['output']);
+        }
+
+    }
+
 }
