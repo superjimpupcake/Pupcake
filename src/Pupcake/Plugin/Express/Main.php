dea278095326a67d72a730271aa7760899b6cb27
jim huang <jim.javathunderbird@gmail.com>
2012-07-06 00:23:44 -0700

    fixed response toRoute issue

--- src/Pupcake/Plugin/Express/Main.php
+++ src/Pupcake/Plugin/Express/Main.php
@@ -9,74 +9,74 @@ use Pupcake;
 class Main extends Pupcake\Plugin
 {
 
-    private $route_map;
+  private $route_map;
 
-    public function setRouteMapToLookup($route_map)
-    {
-        $this->route_map = $route_map;
-    }
+  public function setRouteMapToLookup($route_map)
+  {
+    $this->route_map = $route_map;
+  }
 
-    public function getRouteMapToLookup()
-    {
-        return $this->route_map;
-    }
+  public function getRouteMapToLookup()
+  {
+    return $this->route_map;
+  }
 
-    public function getNextRouteFinder($route, $req, $res)
-    {
-        $plugin = $this;
-        $next = function() use ($route, $req, $res, $plugin) { //find the next matching route
+  public function getNextRouteFinder($route, $req, $res)
+  {
+    $plugin = $this;
+    $next = function() use ($route, $req, $res, $plugin) { //find the next matching route
 
-            $route_map = array();
+      $route_map = array();
 
-            if($plugin->getRouteMapToLookup() === NULL){
-                $route_map = $plugin->getAppInstance()->getRouter()->getRouteMap();
-            }
-            else{
-                $route_map = $plugin->getRouteMapToLookup();
-            }
+      if($plugin->getRouteMapToLookup() === NULL){
+        $route_map = $plugin->getAppInstance()->getRouter()->getRouteMap();
+      }
+      else{
+        $route_map = $plugin->getRouteMapToLookup();
+      }
 
-            $current_route = $route;
-            $current_route_request_type = $current_route->getRequestType();
-            $current_route_pattern = $current_route->getPattern();
+      $current_route = $route;
+      $current_route_request_type = $current_route->getRequestType();
+      $current_route_pattern = $current_route->getPattern();
 
-            //unset the current route 
-            unset($route_map[$current_route_request_type][$current_route_pattern]);
-            $plugin->setRouteMapToLookup($route_map);
+      //unset the current route 
+      unset($route_map[$current_route_request_type][$current_route_pattern]);
+      $plugin->setRouteMapToLookup($route_map);
 
-            $output = ""; //return empty response by default
+      $output = ""; //return empty response by default
 
-            $request_matched = $plugin->getAppInstance()->getRouter()->findMatchedRoute($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO'], $route_map);
+      $request_matched = $plugin->getAppInstance()->getRouter()->findMatchedRoute($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO'], $route_map);
 
-            if($request_matched){ //we found the route
-                $matched_route = $plugin->getAppInstance()->getRouter()->getMatchedRoute();
-                $req->setRoute($matched_route);
-                $next = $plugin->getNextRouteFinder($matched_route, $req, $res);
-                if(is_callable($next)){
-                    $output = $matched_route->execute(array($req, $res, $next)); //execute route and override params
-                }
-            }
+      if($request_matched){ //we found the route
+        $matched_route = $plugin->getAppInstance()->getRouter()->getMatchedRoute();
+        $req->setRoute($matched_route);
+        $next = $plugin->getNextRouteFinder($matched_route, $req, $res);
+        if(is_callable($next)){
+          $output = $matched_route->execute(array($req, $res, $next)); //execute route and override params
+        }
+      }
 
-            return $output;
+      return $output;
 
-        };
+    };
 
-        return $next;
-    }
+    return $next;
+  }
 
-    public function load($config = array())
-    {
-        $this->route_map = null; //initially, set route map to null
+  public function load($config = array())
+  {
+    $this->route_map = null; //initially, set route map to null
 
-        $plugin = $this;
-        $this->on("system.request.found", function($event) use ($plugin) {
-            $route = $event->props('route');
-            $req = new Request($plugin, $route);
-            $res = new Response($plugin, $route, $req);
-            $next = $plugin->getNextRouteFinder($route, $req, $res);
-            if(is_callable($next)){
-                $route->execute(array($req, $res, $next)); //execuite route and override params
-            }
-            return $route->storageGet('output');
-        });
-    }
+    $plugin = $this;
+    $this->on("system.request.found", function($event) use ($plugin) {
+      $route = $event->props('route');
+      $req = new Request($plugin, $route);
+      $res = new Response($plugin, $route, $req);
+      $next = $plugin->getNextRouteFinder($route, $req, $res);
+      if(is_callable($next)){
+        $route->execute(array($req, $res, $next)); //execute route and override params
+      }
+      return $route->storageGet('output');
+    });
+  }
 }
--- src/Pupcake/Plugin/Express/Response.php
+++ src/Pupcake/Plugin/Express/Response.php
@@ -5,46 +5,57 @@ use Pupcake;
 
 class Response extends Pupcake\Object
 {
-    private $plugin;
-    private $route;
-    private $req;
+  private $plugin;
+  private $route;
+  private $req;
+  private $in_inner_route;
 
-    public function __construct($plugin, $route, $req)
-    {
-        $this->plugin = $plugin;
-        $this->route = $route;
-        $this->req = $req;
-        $plugin->trigger("pupcake.plugin.express.response.create", "", array("response" => $this));
+  public function __construct($plugin, $route, $req)
+  {
+    $this->plugin = $plugin;
+    $this->route = $route;
+    $this->req = $req;
+    $this->in_inner_route = false;
+    $plugin->trigger("pupcake.plugin.express.response.create", "", array("response" => $this));
+  }
+
+  public function send($output)
+  {
+    if($this->in_inner_route){
+      $this->route->storageSet('inner_route_output', $output); 
     }
-
-    public function send($output)
-    {
-        $this->route->storageSet('output', $output); 
+    else{
+      $this->route->storageSet('output', $output); 
     }
+  }
 
-    public function redirect($uri)
-    {
-        $this->plugin->getAppInstance()->redirect($uri);
-    }
+  public function redirect($uri)
+  {
+    $this->plugin->getAppInstance()->redirect($uri);
+  }
 
-    public function forward($request_type, $uri)
-    {
-        $this->plugin->getAppInstance()->forward($request_type, $uri);
-        $route = $this->plugin->getAppInstance()->getRouter()->getMatchedRoute();
-        return $route->storageGet('output');
-    }
+  public function forward($request_type, $uri)
+  {
+    $this->plugin->getAppInstance()->forward($request_type, $uri);
+    $route = $this->plugin->getAppInstance()->getRouter()->getMatchedRoute();
+    return $route->storageGet('output');
+  }
 
-    public function toRoute($request_type, $route_pattern, $params)
-    {
-        $router = $this->plugin->getAppInstance()->getRouter();
-        $route = $router->getRoute($request_type, $route_pattern);
-        $route->setParams($params);
-        $this->req->setRoute($route);
-        ob_start();
-        $route->execute(array($this->req, $this));
-        $output = ob_get_contents();
-        ob_end_clean();
-        return $output;
-    } 
+  public function toRoute($request_type, $route_pattern, $params)
+  {
+    $this->in_inner_route = true;
+    $router = $this->plugin->getAppInstance()->getRouter();
+    $route = $router->getRoute($request_type, $route_pattern);
+    $route->setParams($params);
+    $this->req->setRoute($route);
+    $route->execute(array($this->req, $this));
+    $this->in_inner_route = false;
+    return $this->route->storageGet('inner_route_output');
+  }
+
+  public function inInnerRoute()
+  {
+    return $this->in_inner_route;
+  }
 }
 
