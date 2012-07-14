8ed0cda202ad0c4c3dce41e659851afed35d8bef
jim huang <jim.javathunderbird@gmail.com>
2012-07-13 23:14:33 -0700

    allow overriding body value

--- src/Pupcake/Plugin/Express/Request.php
+++ src/Pupcake/Plugin/Express/Request.php
@@ -58,16 +58,21 @@ class Request extends Pupcake\Object
         return $this->args[$index];
     }
 
-    public function body($key = "")
+    public function body($key = "", $value = null)
     {
       $request_method = $_SERVER['REQUEST_METHOD'];
-      if($key == ""){
-        return $GLOBALS["_$request_method"];
+      if($value === NULL){
+        if($key == ""){
+          return $GLOBALS["_$request_method"];
+        }
+        else{
+          if(isset($GLOBALS["_$request_method"][$key])){
+            return $GLOBALS["_$request_method"][$key];
+          }
+        }
       }
       else{
-        if(isset($GLOBALS["_$request_method"][$key])){
-          return $GLOBALS["_$request_method"][$key];
-        }
+        $GLOBALS["_$request_method"][$key] = $value;
       }
     }
 }
