82701ad85e2b9aaa1f201eec9cd105f5e9e156ff
jim huang <jim.javathunderbird@gmail.com>
2012-08-29 23:28:23 -0700

    allow process.nextTick to handle large number of tick callbacks

--- src/Pupcake/Plugin/Node/GlobalObject/Process.php
+++ src/Pupcake/Plugin/Node/GlobalObject/Process.php
@@ -8,17 +8,12 @@ class Process extends GlobalObject
 
   public function nextTick($callback)
   {
-    $this->tick_callback = $callback;
     $loop = $this->getNode()->getEventLoop();
 
-    if(!isset($this->tick)){
-      $this->tick = uv_async_init($loop, array($this, 'tickAsyncCallback'));
-    }
-    uv_async_send($this->tick);
-  }
+    $this->tick = uv_async_init($loop, function($r, $status) use ($callback){
+      $callback();
+    });
 
-  private function tickAsyncCallback($r, $status){
-    $tick_callback = $this->tick_callback;
-    $tick_callback();
+    uv_async_send($this->tick);
   }
 }
