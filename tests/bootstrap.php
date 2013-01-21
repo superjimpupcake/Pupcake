4ab8794320deb630d08928e542044d37572e36d2
superjimpupcake <jimpupcakeframeworkgithub@gmail.com>
2013-01-20 23:42:14 -0800

    code style improvement

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,9 @@ function getAutoLoader()
     $relative_dir = "/";
     $found_autoloader = false;
     $autoloader_path = "";
-    for($k=1;$k<=10;$k++){
+    for ($k=1;$k<=10;$k++ ) {
         $autoloader_path = __DIR__.$relative_dir."vendor/autoload.php";
-        if(is_readable($autoloader_path)){
+        if (is_readable($autoloader_path)) {
             $found_autoloader = true;
             break;
         }
@@ -15,7 +15,7 @@ function getAutoLoader()
         }
     }
 
-    if($found_autoloader){
+    if ($found_autoloader) {
         require $autoloader_path;
     }
 }
