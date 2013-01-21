e31976eddf71dcae9da391d5a4f341c6e1d26b9c
superjimpupcake <jimpupcakeframeworkgithub@gmail.com>
2013-01-20 20:19:00 -0800

    spacing update

--- src/Pupcake/Error.php
+++ src/Pupcake/Error.php
@@ -3,36 +3,36 @@ namespace Pupcake;
 
 class Error extends Object
 {
-  private $severity;
-  private $message;
-  private $file_path;
-  private $line;
+    private $severity;
+    private $message;
+    private $file_path;
+    private $line;
 
-  public function __construct($severity, $message, $file_path, $line)
-  {
-    $this->severity = $severity;
-    $this->message = $message;
-    $this->file_path = $file_path;
-    $this->line = $line;
-  }
+    public function __construct($severity, $message, $file_path, $line)
+    {
+        $this->severity = $severity;
+        $this->message = $message;
+        $this->file_path = $file_path;
+        $this->line = $line;
+    }
 
-  public function getSeverity()
-  {
-    return $this->severity;
-  }
+    public function getSeverity()
+    {
+        return $this->severity;
+    }
 
-  public function getMessage()
-  {
-    return $this->message;
-  }
+    public function getMessage()
+    {
+        return $this->message;
+    }
 
-  public function getFilePath()
-  {
-    return $this->file_path;
-  }
+    public function getFilePath()
+    {
+        return $this->file_path;
+    }
 
-  public function getLine()
-  {
-    return $this->line;
-  }
+    public function getLine()
+    {
+        return $this->line;
+    }
 }
--- src/Pupcake/Plugin/Express/Response.php
+++ src/Pupcake/Plugin/Express/Response.php
@@ -5,74 +5,74 @@ use Pupcake;
 
 class Response extends Pupcake\Object
 {
-  private $plugin;
-  private $app_instance;
-  private $route;
-  private $req;
-  private $in_inner_route;
+    private $plugin;
+    private $app_instance;
+    private $route;
+    private $req;
+    private $in_inner_route;
 
-  public function __construct($plugin, $route, $req)
-  {
-    $this->plugin = $plugin;
-    $this->app_instance = $plugin->getAppInstance();
-    $this->route = $route;
-    $this->req = $req;
-    $this->in_inner_route = false;
-    $plugin->trigger("pupcake.plugin.express.response.create", "", array("response" => $this));
-  }
-
-  public function getAppInstance()
-  {
-    return $this->app_instance;
-  }
-
-  public function send($output)
-  {
-    if($this->in_inner_route){
-      $this->plugin->storageSet('inner_route_output', $output); 
+    public function __construct($plugin, $route, $req)
+    {
+        $this->plugin = $plugin;
+        $this->app_instance = $plugin->getAppInstance();
+        $this->route = $route;
+        $this->req = $req;
+        $this->in_inner_route = false;
+        $plugin->trigger("pupcake.plugin.express.response.create", "", array("response" => $this));
     }
-    else{
-      $this->plugin->storageSet('output', $output); 
+
+    public function getAppInstance()
+    {
+        return $this->app_instance;
     }
-  }
 
-  public function sendJSON($data = array())
-  {
-    $this->contentType("application/json")->send(json_encode($data));
-  }
+    public function send($output)
+    {
+        if ($this->in_inner_route) {
+            $this->plugin->storageSet('inner_route_output', $output); 
+        }
+        else{
+            $this->plugin->storageSet('output', $output); 
+        }
+    }
 
-  public function contentType($content_type)
-  {
-    $this->app_instance->setHeader("Content-type: $content_type");
-    return $this;
-  }
+    public function sendJSON($data = array())
+    {
+        $this->contentType("application/json")->send(json_encode($data));
+    }
 
-  public function redirect($uri)
-  {
-    $this->plugin->getAppInstance()->redirect($uri);
-  }
+    public function contentType($content_type)
+    {
+        $this->app_instance->setHeader("Content-type: $content_type");
+        return $this;
+    }
 
-  public function forward($request_type, $uri, $request_params = array())
-  {
-    return $this->app_instance->forward($request_type, $uri, $request_params);
-  }
+    public function redirect($uri)
+    {
+        $this->plugin->getAppInstance()->redirect($uri);
+    }
 
-  public function toRoute($request_type, $route_pattern, $params)
-  {
-    $this->in_inner_route = true;
-    $router = $this->plugin->getAppInstance()->getRouter();
-    $route = $router->getRoute($request_type, $route_pattern);
-    $route->setParams($params);
-    $this->req->setRoute($route);
-    $route->execute(array($this->req, $this));
-    $this->req->setRoute($this->route); //set back the request route
-    $this->in_inner_route = false;
-    return $this->plugin->storageGet('inner_route_output');
-  }
+    public function forward($request_type, $uri, $request_params = array())
+    {
+        return $this->app_instance->forward($request_type, $uri, $request_params);
+    }
 
-  public function inInnerRoute()
-  {
-    return $this->in_inner_route;
-  }
+    public function toRoute($request_type, $route_pattern, $params)
+    {
+        $this->in_inner_route = true;
+        $router = $this->plugin->getAppInstance()->getRouter();
+        $route = $router->getRoute($request_type, $route_pattern);
+        $route->setParams($params);
+        $this->req->setRoute($route);
+        $route->execute(array($this->req, $this));
+        $this->req->setRoute($this->route); //set back the request route
+        $this->in_inner_route = false;
+        return $this->plugin->storageGet('inner_route_output');
+    }
+
+    public function inInnerRoute()
+    {
+        return $this->in_inner_route;
+    }
 }
 
