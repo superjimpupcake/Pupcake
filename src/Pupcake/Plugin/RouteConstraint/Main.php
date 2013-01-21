e98c67b027d0ca14e04075d3ede419b73a759782
superjimpupcake <jimpupcakeframeworkgithub@gmail.com>
2013-01-20 20:26:23 -0800

    code style update

--- src/Pupcake/Plugin/RouteConstraint/Main.php
+++ src/Pupcake/Plugin/RouteConstraint/Main.php
@@ -14,9 +14,9 @@ class Main extends Pupcake\Plugin
          * When a route object is being created, we add the constraint method 
          * to it and store the constraint into this route object's storage
          */
-        $this->help("system.routing.route.create", function($event){
+        $this->help("system.routing.route.create", function($event) {
             $route = $event->props('route');
-            $route->method('constraint', function($constraint) use($route){
+            $route->method('constraint', function($constraint) use($route) {
                 $route->storageSet('constraint', $constraint);
                 return $route; //return the route reference for futher extension
             });
@@ -26,15 +26,15 @@ class Main extends Pupcake\Plugin
          * When a route object is initially matched, we add further checking logic 
          * to make sure the constraint is applying toward the route matching process
          */
-        $this->help("system.routing.route.matched", function($event){
+        $this->help("system.routing.route.matched", function($event) {
             $route = $event->props('route');
             $matched = true;
             $params = $route->getParams();
             $constraint = $route->storageGet('constraint');
-            if(count($constraint) > 0){
-                foreach($constraint as $token => $validation_callback){
-                    if(is_callable($validation_callback)){
-                        if(!$validation_callback($params[$token])){
+            if (count($constraint) > 0) {
+                foreach ($constraint as $token => $validation_callback) {
+                    if (is_callable($validation_callback)) {
+                        if (!$validation_callback($params[$token])) {
                             $matched = false;
                             break;
                         }
